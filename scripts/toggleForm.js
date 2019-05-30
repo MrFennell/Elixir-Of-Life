a6ef5d0d666357930878e48156bc54f9fa35e36b
Ryan <rfennell001@gmail.com>
2019-05-30 18:01:19 -0400

    Form script and styles added

---
+++ scripts/toggleForm.js
@@ -0,0 +1,14 @@
+function showFormPt2(){
+    console.log('shown');
+    document.getElementById('requestInfo-Block2').style.display = "block";
+    document.getElementById('please-check').style.display = "none";
+    document.getElementById('submitForm').style.display = "block";
+    
+}
+function hideFormPt2(){
+    console.log('hide');
+    document.getElementById('requestInfo-Block2').style.display = "none";
+    document.getElementById('please-check').style.display = "none";
+    document.getElementById('submitForm').style.display = "block";
+
+}
