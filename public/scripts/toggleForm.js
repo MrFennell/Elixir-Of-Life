6eca32951651771953ab0a01ebefbdd370c87171
Ryan <rfennell001@gmail.com>
2019-08-08 17:16:33 -0400

    Animated form sections, added favicon and style changes

--- public/scripts/toggleForm.js
+++ public/scripts/toggleForm.js
@@ -1,27 +1,48 @@
-function showFormPt2(){
-    document.getElementById('form-block-2').style.display = "block";
-    // document.getElementById('form-additional-info-1').style.display = "none";
-    // document.getElementById('submit').style.display = "block";
 
-}
-function hideFormPt2(){
-    document.getElementById('form-block-2').style.display = "none";
-    // document.getElementById('please-check').style.display = "none";
-    // document.getElementById('submit').style.display = "block";
-}
-function showFormPt3(){
-    document.getElementById('form-block-3').style.display = "block";
-    // document.getElementById('please-check').style.display = "none";
-}
-function hideFormPt3(){
-    document.getElementById('form-block-3').style.display = "none";
+document.querySelector('#additional-info-yes').addEventListener('click', function(e){
+   var el =  document.querySelector('#form-block-2');
+    el.classList.add('display');
+        setTimeout(() => {
+            el.classList.add('visible');
+        }, 1);
+});
 
-}
-function showFormPt4(){
-    document.getElementById('form-block-4').style.display = "block";
-    // document.getElementById('please-check').style.display = "none";
-}
-function hideFormPt4(){
-    document.getElementById('form-block-4').style.display = "none";
+document.querySelector('#additional-info-no').addEventListener('click', function(e){
+    var el =  document.querySelector('#form-block-2');
+    el.classList.remove('visible');
+        setTimeout(() => {
+            el.classList.remove('display');
+        }, 500);
+});
+  
+document.querySelector('#additional-info-yes-2').addEventListener('click', function(e){
+    var el =  document.querySelector('#form-block-3');
+     el.classList.add('display');
+         setTimeout(() => {
+             el.classList.add('visible');
+         }, 1);
+ });
+ 
+ document.querySelector('#additional-info-no-2').addEventListener('click', function(e){
+     var el =  document.querySelector('#form-block-3');
+     el.classList.remove('visible');
+         setTimeout(() => {
+             el.classList.remove('display');
+         }, 500);
+ });
 
-}
+ document.querySelector('#additional-info-yes-3').addEventListener('click', function(e){
+    var el =  document.querySelector('#form-block-4');
+     el.classList.add('display');
+         setTimeout(() => {
+             el.classList.add('visible');
+         }, 1);
+ });
+ 
+ document.querySelector('#additional-info-no-3').addEventListener('click', function(e){
+     var el =  document.querySelector('#form-block-4');
+     el.classList.remove('visible');
+         setTimeout(() => {
+             el.classList.remove('display');
+         }, 500);
+ });
